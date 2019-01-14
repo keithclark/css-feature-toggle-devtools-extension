a5716e607f7f5fa0a4160c14ae5cca22969575df
Keith Clark <keith@keithclark.co.uk>
2019-01-14 20:12:11 +0000

    Add support for `calc()` function

---
+++ panel/features/calc.js
@@ -0,0 +1,10 @@
+import {functionNameOption} from '../feature-helpers.js';
+
+export default functionNameOption({
+  name: 'Mathematical expressions',
+  group: 'Other',
+  help: 'Disable support for the `calc()` function',
+  functionNames: [
+    'calc'
+  ]
+});
