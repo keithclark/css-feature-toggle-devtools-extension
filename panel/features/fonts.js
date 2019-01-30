218359488d39ed52ea9d5383c4dcc1c11d61668d
Keith Clark <keith@keithclark.co.uk>
2019-01-30 23:25:24 +0000

    Update custom font option text

--- panel/features/fonts.js
+++ panel/features/fonts.js
@@ -3,6 +3,6 @@ import {atRuleIdentifierOption} from '../feature-helpers.js';
 export default atRuleIdentifierOption({
   name: 'Custom fonts',
   group: 'Other',
-  help: 'Disable support for custom fonts defined with `@font-face`',
+  help: 'Disable loading of custom fonts with `@font-face`',
   identifier: 'font-face'
 });
