c652b9b532d08216af62b1710aceccb18eb763d8
Keith Clark <keith@keithclark.co.uk>
2020-02-24 22:24:13 +0000

    Feature: Scroll Behaviour

---
+++ extension/panel/features/scroll-behavior.js
@@ -0,0 +1,10 @@
+import {propertyNameOption} from '../feature-helpers.js';
+
+export default propertyNameOption({
+  name: 'Scroll behavior',
+  group: 'Other',
+  help: 'Disable support for scroll behavior',
+  propertyNames: [
+    'scroll-behavior'
+  ]
+});
