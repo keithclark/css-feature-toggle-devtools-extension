7da1fdf5630962dada7e4637abac9c94e5d8c465
Andrew Viney <2415212+itsViney@users.noreply.github.com>
2022-03-03 13:22:21 +0000

    Add files via upload
    Add ability to disable support for 'aspect-ratio' property

---
+++ extension/panel/features/aspect-ratio.js
@@ -0,0 +1,9 @@
+import { propertyNameOption } from '../feature-helpers.js';
+
+export default propertyNameOption({
+    name: 'Aspect Ratio',
+    group: 'Content Layout',
+    propertyNames: [
+        'aspect-ratio'
+    ]
+});
